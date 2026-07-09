b4cd45d20b28565a9e673d8be82affb956d65783
damstrivandrum <aajalms@gmail.com>
2026-07-09 12:53:36 +0530

    Update image path for question ID 11

--- questions.js
+++ questions.js
@@ -2,7 +2,7 @@
 window.questions = [
   {
     id: 11,
-    image: "",
+    image: "images/ScreenRecording_07-09-2026 12-51-04_1.mov",
     question: "A video shows a patient walking with a wide-based, unsteady gait. Which structure is involved?",
     options: [
       "Basal ganglia",
